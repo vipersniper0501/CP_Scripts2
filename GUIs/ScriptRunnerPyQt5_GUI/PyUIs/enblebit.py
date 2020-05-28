eccde791fd023ad08a2619d05b74126c901e4da1
Viper <56606800+vipersniper0501@users.noreply.github.com>
2020-05-28 17:30:43 +0200

    version 0.5.35.0

--- GUIs/ScriptRunnerPyQt5_GUI/PyUIs/enblebit.py
+++ GUIs/ScriptRunnerPyQt5_GUI/PyUIs/enblebit.py
@@ -13,17 +13,17 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_bitlockerGUI(object):
     def setupUi(self, bitlockerGUI):
         bitlockerGUI.setObjectName("bitlockerGUI")
-        bitlockerGUI.resize(483, 292)
+        bitlockerGUI.resize(483, 323)
         bitlockerGUI.setStyleSheet("background-color: #212434\n"
-"")
+                                   "")
         self.frame = QtWidgets.QFrame(bitlockerGUI)
-        self.frame.setGeometry(QtCore.QRect(10, 40, 160, 241))
+        self.frame.setGeometry(QtCore.QRect(15, 70, 160, 241))
         self.frame.setStyleSheet("border: 2px solid;\n"
-"border-width: 3px;\n"
-"background: transparent;\n"
-"border-radius: 10px; \n"
-"color: black;\n"
-"padding: 3px;")
+                                 "border-width: 3px;\n"
+                                 "background: transparent;\n"
+                                 "border-radius: 10px; \n"
+                                 "color: black;\n"
+                                 "padding: 3px;")
         self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame.setObjectName("frame")
@@ -67,28 +67,28 @@ class Ui_bitlockerGUI(object):
         self.radioButton_7.setText("")
         self.radioButton_7.setObjectName("radioButton_7")
         self.enblBIT = QtWidgets.QPushButton(bitlockerGUI)
-        self.enblBIT.setGeometry(QtCore.QRect(189, 40, 281, 131))
+        self.enblBIT.setGeometry(QtCore.QRect(194, 70, 281, 131))
         font = QtGui.QFont()
         font.setPointSize(28)
         self.enblBIT.setFont(font)
         self.enblBIT.setStyleSheet("background-color: #414E6E;color: #CCD2E6;")
         self.enblBIT.setObjectName("enblBIT")
         self.label_2 = QtWidgets.QLabel(bitlockerGUI)
-        self.label_2.setGeometry(QtCore.QRect(10, 10, 101, 21))
+        self.label_2.setGeometry(QtCore.QRect(10, 10, 121, 21))
         self.label_2.setStyleSheet("color: #8B93B2;")
         self.label_2.setObjectName("label_2")
         self.encrypPASS = QtWidgets.QLineEdit(bitlockerGUI)
-        self.encrypPASS.setGeometry(QtCore.QRect(120, 10, 321, 20))
+        self.encrypPASS.setGeometry(QtCore.QRect(150, 10, 311, 20))
         self.encrypPASS.setStyleSheet("background-color: #414E6E;color: #CCD2E6;border: none;")
         self.encrypPASS.setObjectName("encrypPASS")
         self.frame_2 = QtWidgets.QFrame(bitlockerGUI)
-        self.frame_2.setGeometry(QtCore.QRect(180, 180, 291, 61))
+        self.frame_2.setGeometry(QtCore.QRect(185, 210, 291, 61))
         self.frame_2.setStyleSheet("border: 2px solid;\n"
-"border-width: 3px;\n"
-"background: transparent;\n"
-"border-radius: 10px; \n"
-"color: black;\n"
-"padding: 3px;")
+                                   "border-width: 3px;\n"
+                                   "background: transparent;\n"
+                                   "border-radius: 10px; \n"
+                                   "color: black;\n"
+                                   "padding: 3px;")
         self.frame_2.setFrameShape(QtWidgets.QFrame.StyledPanel)
         self.frame_2.setFrameShadow(QtWidgets.QFrame.Raised)
         self.frame_2.setObjectName("frame_2")
@@ -108,15 +108,33 @@ class Ui_bitlockerGUI(object):
         self.label_4.setText("")
         self.label_4.setObjectName("label_4")
         self.cancelbutton = QtWidgets.QPushButton(bitlockerGUI)
-        self.cancelbutton.setGeometry(QtCore.QRect(315, 250, 151, 31))
+        self.cancelbutton.setGeometry(QtCore.QRect(320, 280, 151, 31))
         font = QtGui.QFont()
         font.setPointSize(15)
         self.cancelbutton.setFont(font)
         self.cancelbutton.setStyleSheet("background-color: #414E6E;color: #CCD2E6;")
         self.cancelbutton.setObjectName("cancelbutton")
+        self.encrypPASS_2 = QtWidgets.QLineEdit(bitlockerGUI)
+        self.encrypPASS_2.setGeometry(QtCore.QRect(150, 40, 311, 20))
+        self.encrypPASS_2.setStyleSheet("background-color: #414E6E;color: #CCD2E6;border: none;")
+        self.encrypPASS_2.setObjectName("encrypPASS_2")
+        self.label_5 = QtWidgets.QLabel(bitlockerGUI)
+        self.label_5.setGeometry(QtCore.QRect(10, 40, 141, 21))
+        self.label_5.setStyleSheet("color: #8B93B2;")
+        self.label_5.setObjectName("label_5")
 
         self.retranslateUi(bitlockerGUI)
         QtCore.QMetaObject.connectSlotsByName(bitlockerGUI)
+        bitlockerGUI.setTabOrder(self.encrypPASS, self.encrypPASS_2)
+        bitlockerGUI.setTabOrder(self.encrypPASS_2, self.radioButton)
+        bitlockerGUI.setTabOrder(self.radioButton, self.radioButton_2)
+        bitlockerGUI.setTabOrder(self.radioButton_2, self.radioButton_3)
+        bitlockerGUI.setTabOrder(self.radioButton_3, self.radioButton_4)
+        bitlockerGUI.setTabOrder(self.radioButton_4, self.radioButton_5)
+        bitlockerGUI.setTabOrder(self.radioButton_5, self.radioButton_6)
+        bitlockerGUI.setTabOrder(self.radioButton_6, self.radioButton_7)
+        bitlockerGUI.setTabOrder(self.radioButton_7, self.enblBIT)
+        bitlockerGUI.setTabOrder(self.enblBIT, self.cancelbutton)
 
     def retranslateUi(self, bitlockerGUI):
         _translate = QtCore.QCoreApplication.translate
@@ -126,6 +144,7 @@ class Ui_bitlockerGUI(object):
         self.label_2.setText(_translate("bitlockerGUI", "Encryption Password:"))
         self.label_3.setText(_translate("bitlockerGUI", "Encryption Status:"))
         self.cancelbutton.setText(_translate("bitlockerGUI", "Cancel"))
+        self.label_5.setText(_translate("bitlockerGUI", "Verify Encryption Password:"))
 
 
 if __name__ == "__main__":
--- GUIs/ScriptRunnerPyQt5_GUI/scripFunc/scripWINONLY.py
+++ GUIs/ScriptRunnerPyQt5_GUI/scripFunc/scripWINONLY.py
@@ -2,6 +2,7 @@ import os
 import subprocess as sub
 from sys import platform
 import getpass
+import itertools
 from threading import *
 import distro  # for figuring out what linux distro
 import configparser
@@ -19,6 +20,7 @@ ops = OS[0]
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
@@ -28,6 +30,7 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
+
 
 '''
 This file is used to store commands that are to only be used on Windows machines
@@ -47,25 +50,140 @@ class funcWINONLY:
             def EXECUTE(self):
 
                 # Executes encryption command
-                def ENCRYPT(x):
-                    if self.encrypPASS.text() == '':
+                def ENCRYPT(drive):
+                    l1 = self.encrypPASS.text()
+                    # print(l1)
+                    # print(len(l1))
+                    l2 = self.encrypPASS_2.text()
+                    # print(l2)
+                    # print(len(l2))
+                    # Try loop enforces password policy.
+                    try:
+                        symbols = '`~!@#$%^&*()_+-=[]{};:,./<>?'
+                        characterBOOL = ''
+                        for i, x in itertools.product(range(0, len(l1)), range(0, len(l2))):
+                            character = l1[i]
+                            character2 = l2[x]
+                            if character.islower() and character2.islower():
+                                characterBOOL = True
+                            elif not character.islower() and not character2.islower():
+                                characterBOOL = False
+                            if characterBOOL:
+                                break
+
+                        characterUPBOOL = ''
+                        for i, x in itertools.product(range(0, len(l1)), range(0, len(l2))):
+                            character = l1[i]
+                            character2 = l2[x]
+                            if character.isupper() and character2.isupper():
+                                characterUPBOOL = True
+                            elif not character.isupper() and not character2.isupper():
+                                characterUPBOOL = False
+                            if characterUPBOOL:
+                                break
+
+                        symbolsBOOL = ''
+                        for y in range(0, len(symbols)):
+                            if (symbols[y] not in l1) and (symbols[y] not in l2):
+                                symbolsBOOL = False
+                            elif (symbols[y] in l1) and (symbols[y] in l2):
+                                symbolsBOOL = True
+                            if symbolsBOOL:
+                                break
+
+                        numberBOOL = ''
+                        for i, x in itertools.product(range(0, len(l1)), range(0, len(l2))):
+                            character = l1[i]
+                            character2 = l2[x]
+                            if character.isdigit() and character2.isdigit():
+                                numberBOOL = True
+                            elif not character.isdigit() and not character2.isdigit():
+                                numberBOOL = False
+                            if numberBOOL:
+                                break
+
+                        print(str(characterBOOL) + ' Lower Case')
+                        print(str(characterUPBOOL) + ' Upper case')
+                        print(str(symbolsBOOL) + ' symbols')
+                        print(str(numberBOOL) + ' number')
+                    except IndexError:
+                        HEY = QMessageBox()
+                        HEY.setWindowTitle('Hey! Listen!')
+                        HEY.setText("Hey! Your passwords do not match!")
+                        HEY.setIcon(QMessageBox.Critical)
+                        HEY.setWindowIcon(QtGui.QIcon(':/Pictures/images/HEY.png'))
+                        x = HEY.exec_()
+
+                    if len(l1) == 0 or len(l2) == 0:
                         HEY = QMessageBox()
                         HEY.setWindowTitle('Hey! Listen!')
                         HEY.setText("Hey! You don't have a password!")
                         HEY.setIcon(QMessageBox.Critical)
                         HEY.setWindowIcon(QtGui.QIcon(':/Pictures/images/HEY.png'))
                         x = HEY.exec_()
+                    elif len(l1) <= 8 or len(l2) <= 8:
+                        HEY = QMessageBox()
+                        HEY.setWindowTitle('Hey! Listen!')
+                        HEY.setText("Hey! Your password must have at least 8 characters!")
+                        HEY.setIcon(QMessageBox.Critical)
+                        HEY.setWindowIcon(QtGui.QIcon(':/Pictures/images/HEY.png'))
+                        x = HEY.exec_()
+                    elif l1 != l2:
+                        HEY = QMessageBox()
+                        HEY.setWindowTitle('Hey! Listen!')
+                        HEY.setText("Hey! Your passwords do not match!")
+                        HEY.setIcon(QMessageBox.Critical)
+                        HEY.setWindowIcon(QtGui.QIcon(':/Pictures/images/HEY.png'))
+                        x = HEY.exec_()
+                    elif not characterBOOL:
+                        HEY = QMessageBox()
+                        HEY.setWindowTitle('Hey! Listen!')
+                        HEY.setText("Hey! Your password must have at least 1 lower case letter!")
+                        HEY.setIcon(QMessageBox.Critical)
+                        HEY.setWindowIcon(QtGui.QIcon(':/Pictures/images/HEY.png'))
+                        x = HEY.exec_()
+                    elif not characterUPBOOL:
+                        HEY = QMessageBox()
+                        HEY.setWindowTitle('Hey! Listen!')
+                        HEY.setText("Hey! Your password must have at least 1 Upper Case letter!")
+                        HEY.setIcon(QMessageBox.Critical)
+                        HEY.setWindowIcon(QtGui.QIcon(':/Pictures/images/HEY.png'))
+                        x = HEY.exec_()
+                    elif not symbolsBOOL:
+                        HEY = QMessageBox()
+                        HEY.setWindowTitle('Hey! Listen!')
+                        HEY.setText("Hey! Your password must have at least 1 Symbol! [Ex: !@#$%^%&]")
+                        HEY.setIcon(QMessageBox.Critical)
+                        HEY.setWindowIcon(QtGui.QIcon(':/Pictures/images/HEY.png'))
+                        x = HEY.exec_()
+                    elif not numberBOOL:
+                        HEY = QMessageBox()
+                        HEY.setWindowTitle('Hey! Listen!')
+                        HEY.setText("Hey! Your password must have at least 1 number! [Ex: !@#$%^%&]")
+                        HEY.setIcon(QMessageBox.Critical)
+                        HEY.setWindowIcon(QtGui.QIcon(':/Pictures/images/HEY.png'))
+                        x = HEY.exec_()
                     else:
                         command = """
 $pass = ConvertTo-SecureString '""" + self.encrypPASS.text() + """' -AsPlainText -Force
-Enable-BitLocker """ + x + """ -PasswordProtector $pass"""
+Enable-BitLocker """ + drive + """ -PasswordProtector $pass"""
                         '''
                         Only works if 'Allow BitLocker without compatible TPM' is enabled in the Group Policy
                         Also does not encrypt right away. If you type 'Get-BitlockerVolume' in powershell, it will tell
                         you how much has been encrypted so far.
                         '''
                         print(command)
-                        sub.Popen(["powershell", "& {" + command + "}"])
+
+                        # sub.Popen(["powershell", "& {" + command + "}"])
+
+                        def restartyn(i):
+                            print(i)
+
+                        COMPLETE = QMessageBox()
+                        COMPLETE.setIcon(QMessageBox.Question)
+                        COMPLETE.setWindowTitle('Hey! Listen!')
+                        COMPLETE.setText('Restart for encryption to begin on the drive.')
+                        x = COMPLETE.exec_()
 
                 # Gets list of drives and encryption status'
                 decryptSTATUS = []
@@ -86,7 +204,7 @@ Enable-BitLocker """ + x + """ -PasswordProtector $pass"""
                     except Exception as e:
                         # print('Controlled exit of loop: ' + str(e))
                         break
-                print(decryptSTATUS)
+                # print(decryptSTATUS)
                 buttons = [self.radioButton, self.radioButton_2, self.radioButton_3, self.radioButton_4,
                            self.radioButton_5, self.radioButton_6, self.radioButton_7]
 
@@ -111,6 +229,7 @@ Enable-BitLocker """ + x + """ -PasswordProtector $pass"""
                 def cancelbutton():
                     self.close()
 
+                # Assigns buttons to commands
                 self.radioButton.toggled.connect(lambda: STATUSCHECK(driveLETTER[0]))
                 self.radioButton_2.toggled.connect(lambda: STATUSCHECK(driveLETTER[1]))
                 self.radioButton_3.toggled.connect(lambda: STATUSCHECK(driveLETTER[2]))
@@ -163,6 +282,7 @@ Enable-BitLocker """ + x + """ -PasswordProtector $pass"""
                 manage-bde -protectors -add -pw $drv
                 manage-bde -on $drv"""
                 '''
+
         # Allows for program to continue running while the function executes.
         def threader(com):
             try:
