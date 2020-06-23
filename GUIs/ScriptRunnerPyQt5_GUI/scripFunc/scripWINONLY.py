848325a466c2050b5103d9099421a73ceba58b3b
Viper <56606800+vipersniper0501@users.noreply.github.com>
2020-06-23 21:21:16 +0200

    version 3.0.11.2 - Small Bugfixes

--- GUIs/ScriptRunnerPyQt5_GUI/scripFunc/scripWINONLY.py
+++ GUIs/ScriptRunnerPyQt5_GUI/scripFunc/scripWINONLY.py
@@ -19,16 +19,15 @@ ops = OS[0]
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
+# def resource_path(relative_path):
+#     """ Get absolute path to resource, works for dev and for PyInstaller """
+#     try:
+#         # PyInstaller creates a temp folder and stores path in _MEIPASS
+#         base_path = sys._MEIPASS
+#     except Exception:
+#         base_path = os.path.abspath(".")
+#
+#     return os.path.join(base_path, relative_path)
 
 
 '''
