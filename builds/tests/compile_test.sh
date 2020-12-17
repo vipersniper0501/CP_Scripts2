0ec438564b34db7f0b039948579d622e3d5c47b2
vipersniper0501 <michaelabrenner@gmail.com>
2020-12-17 11:02:38 +0100

    travis update 4

--- builds/tests/compile_test.sh
+++ builds/tests/compile_test.sh
@@ -7,7 +7,7 @@ if [ $TRAVIS_OS_NAME = 'osx' ]; then
     case "${TOXENV}" in
         py38)
             # Install some custom Python 3.2 requirements on macOS
-            brew install python@3.8
+            HOMEBREW_NO_AUTO_UPDATE=1 brew install python@3.8
             ;;
     esac
     pyinstaller -y -F -c -i "./GUIs/ScriptRunnerPyQt5_GUI/images/cup2.ico" --name "AppleCIDR_MacOS_x64"  --add-data "./GUIs/ScriptRunnerPyQt5_GUI/configurations":"configurations/" --add-data "./GUIs/ScriptRunnerPyQt5_GUI/images":"images/" --add-data "./GUIs/ScriptRunnerPyQt5_GUI/PyUIs":"PyUIs/" --add-data "./GUIs/ScriptRunnerPyQt5_GUI/scripFunc":"scripFunc/" --add-data "./GUIs/ScriptRunnerPyQt5_GUI/requirements.txt":"." --add-data "./GUIs/ScriptRunnerPyQt5_GUI/resources_rc.py":"." --add-data "./GUIs/ScriptRunnerPyQt5_GUI/resources.qrc":"."  "./GUIs/ScriptRunnerPyQt5_GUI/ScriptGUIrunner.py"
