a50730699705804547703cae898ee32b58924a68
vipersniper0501 <michaelabrenner@gmail.com>
2021-01-14 16:40:22 +0100

    Self Runner testing

--- builds/tests/after_successCONVERT.sh
+++ builds/tests/after_successCONVERT.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 setup_git() {
-    git config --global user.email "travis@travis-ci.org"
-    git config --global user.name "Travis CI"
+    git config --global user.email "github@github-actions.org"
+    git config --global user.name "Runner"
 }
 
 commit_compiled_executables() {
 
-    if [ $TRAVIS_BRANCH = dev ]; then
+    Branch_Name=$(git branch --show-current)
+    Branch_Commit_Number=$(git rev-list --cound $Branch_Name)
+
+    if [ $Branch_Name = dev ]; then
         git checkout dev
         echo "checked out dev"
         echo "./dist directory BEFORE"
         ls ./dist
         echo "./builds directory BEFORE"
         ls ./builds
-        if [ $TRAVIS_OS_NAME = 'windows' ]; then
+        if [ $@ = 'windows' ]; then
             mv "./dist/AppleCIDR_Windows_x64.exe" "./builds/PyQt5_Executables/AppleCIDR_Windows_x64.exe"
             echo "Moved AppleCIDR_Windows_x64.exe from ./dist to ./builds"
             echo "./dist directory AFTER"
@@ -25,7 +28,7 @@ commit_compiled_executables() {
             git add "./builds"
             git status
         fi
-        if [ $TRAVIS_OS_NAME = 'linux' ]; then
+        if [ $@ = 'linux' ]; then
             mv "./dist/AppleCIDR_Linux_x64" "./builds/PyQt5_Executables/AppleCIDR_Linux_x64"
             echo "Moved AppleCIDR_Linux_x64 from ./dist to ./builds"
             echo "./dist directory AFTER"
@@ -37,7 +40,7 @@ commit_compiled_executables() {
             git add "./builds"
             git status
         fi
-        if [ $TRAVIS_OS_NAME = 'osx' ]; then
+        if [ $@ = 'osx' ]; then
             mv "./dist/AppleCIDR_MacOS_x64" "./builds/PyQt5_Executables/AppleCIDR_MacOS_x64"
             echo "Moved AppleCIDR_MacOS_x64 from ./dist to ./builds/PyQt5_Executables/"
             echo "./dist directory AFTER"
@@ -49,16 +52,16 @@ commit_compiled_executables() {
             git add "./builds"
             git status
         fi
-        git commit --message "[skip travis-ci] Travis build: $TRAVIS_BUILD_NUMBER"
+        git commit --message "Branch: $Branch_Name, Build Number [$Branch_Commit_Number]"
         echo "git committed created. Ready to push"
-    elif [ $TRAVIS_BRANCH = master ]; then
+    elif [ $Branch_Name = master ]; then
         git checkout master
         echo "checked out master"
         echo "./dist directory BEFORE"
         ls ./dist
         echo "./builds directory BEFORE"
         ls ./builds
-        if [ $TRAVIS_OS_NAME = 'windows' ]; then
+        if [ $@ = 'windows' ]; then
             mv "./dist/AppleCIDR_Windows_x64.exe" "./builds/PyQt5_Executables/AppleCIDR_Windows_x64.exe"
             echo "Moved AppleCIDR_Windows_x64.exe from ./dist to ./builds"
             echo "./dist directory AFTER"
@@ -69,7 +72,7 @@ commit_compiled_executables() {
             git add "./builds"
             git status
         fi
-        if [ $TRAVIS_OS_NAME = 'linux' ]; then
+        if [ $@ = 'linux' ]; then
             mv "./dist/AppleCIDR_Linux_x64" "./builds/PyQt5_Executables/AppleCIDR_Linux_x64"
             echo "Moved AppleCIDR_Linux_x64 from ./dist to ./builds"
             echo "./dist directory AFTER"
@@ -81,7 +84,7 @@ commit_compiled_executables() {
             git add "./builds"
             git status
         fi
-        if [ $TRAVIS_OS_NAME = 'osx' ]; then
+        if [ $@ = 'osx' ]; then
             mv "./dist/AppleCIDR_MacOS_x64" "./builds/PyQt5_Executables/AppleCIDR_MacOS_x64"
             echo "Moved AppleCIDR_MacOS_x64 from ./dist to ./builds/PyQt5_Executables/"
             echo "./dist directory AFTER"
@@ -93,25 +96,21 @@ commit_compiled_executables() {
             git add "./builds"
             git status
         fi
-        git commit --message "[skip travis-ci] Travis build: $TRAVIS_BUILD_NUMBER"
+        git commit --message "Branch: $Branch_Name, Build Number [$Branch_Commit_Number]"
         echo "git committed created. Ready to push"
     fi
 }
 
 upload_files() {
-    if [ $TRAVIS_BRANCH = dev ]; then
+    if [ $Branch_Name = dev ]; then
         git push -f -q https://vipersniper0501:${GH_TOKEN_DEV}@github.com/vipersniper0501/CP_Scripts2.git dev
         echo "Pushed to dev"
-    elif [ $TRAVIS_BRANCH = master ]; then
+    elif [ $Branch_Name = master ]; then
         git push -f -q https://vipersniper0501:${GH_TOKEN_DEV}@github.com/vipersniper0501/CP_Scripts2.git master
         echo "Pushed to master"
     fi
 }
 
-#if [ $TRAVIS_OS_NAME = 'osx' ]; then
-#    echo "This is not a windows or linux machine. Will not upload to github yet"
-#else
 setup_git
 commit_compiled_executables
 upload_files
-#fi
